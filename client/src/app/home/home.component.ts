9ebff4eefdcb66623d54edd0bb9049c05f3b70b2
congtt <congtt@dft.vn>
2023-09-11 18:37:28 +0700

    End of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,23 +9,14 @@ export class HomeComponent {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.getUser();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
-  }
-
-  getUser() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
   }
 
   cancelRegisterMode(event: boolean) {
