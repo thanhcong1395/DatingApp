23a7a2f286c95445b4fdacdfc8621585cc3543da
thanhcong <congtt@dft.vn>
2023-10-01 23:54:05 +0700

    End of lesson 177 section 14

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,57 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace API.Controllers
+{
+    public class LikesController : BaseApiController
+    {
+        private readonly IUserRepository userRepository;
+        private readonly ILikesRepository likesRepository;
+
+        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        {
+            this.userRepository = userRepository;
+            this.likesRepository = likesRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string username)
+        {
+            var sourceUserId = User.GetUserId();
+            var likedUser = await this.userRepository.GetUserByUsernameAsync(username);
+            var sourceUser = await this.likesRepository.GetUserWithLikes(sourceUserId);
+
+            if (likedUser == null) return NotFound();
+
+            if (sourceUser.UserName == username) return BadRequest("You cannot like yourseft");
+
+            var userLike = await this.likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+            if (userLike != null) return BadRequest("You already like this user");
+
+            userLike = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                TargetUserId = likedUser.Id
+            };
+
+            sourceUser.LikedUsers.Add(userLike);
+
+            if (await this.userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Failed to like user");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+        {
+            var users = await this.likesRepository.GetUserLikes(predicate, User.GetUserId());
+
+            return Ok(users);
+        }
+    }
+}
